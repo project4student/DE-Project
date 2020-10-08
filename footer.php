032d236e0e1ec9055e2f132df77c179f7478fe1f
project4student <72503439+project4student@users.noreply.github.com>
2020-10-08 20:11:44 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="container-fluid bg-dark text-light">
+    <p class="text-center">Copyright RAMC Blood Bank 2021 | All rights reserved</p>
+</div>
---
+++ signupModal.php
@@ -0,0 +1,33 @@
+<!-- Modal -->
+<div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="signupModalLabel">CREATE YOUR ACCOUNT</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                            else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Confirm Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-danger">SignUp</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
